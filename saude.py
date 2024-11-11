2606c162f302d9e066a4cfd12a0e9a4f9134812e
gewllx <gegeoalbuquerque@gmail.com>
2024-11-11 17:46:42 -0300

    meu primeiro commit

---
+++ saude.py
@@ -0,0 +1,6 @@
+# Solicita ao usuário que digite um membro do corpo
+membro_do_corpo = input("Digite um membro do corpo: ")
+
+# Exibe o membro do corpo digitado pelo usuário
+print("O membro do corpo é:", membro_do_corpo)
+
